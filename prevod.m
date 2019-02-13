dd0a25c03be862029e95cfc3d09c191f7764da37
Xendrra <47497023+Xendrra@users.noreply.github.com>
2019-02-13 22:59:32 +0100

    Update prevod.m

--- prevod.m
+++ prevod.m
@@ -1,10 +1,13 @@
 function [protein] = prevod(data)
+%function 'prevod' translates nucleotide sequence from any genbank file to protein sequence
+%input - genbank file
+%output - string with final protein
 data=genbankread(data);
 ind=data.CDS.indices;
 sek='';
 complement='';
 for i=1:length(ind)-1
-    sek=[sek, data.Sequence(ind(i):ind(i+1))];
+    sek=[sek, data.Sequence(ind(i):ind(i+1))]; %its translating only triplets marked by indices
 end
 if ind(end)<ind(i)
     sek=seqrcomplement(sek)
